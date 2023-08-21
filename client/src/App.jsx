e719ed8197bc75b5f069736690cdf4a80d9aa9d3
Rahul <rahulshelke4172@gmail.com>
2023-08-21 23:03:02 +0530

    created components  pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-       <h2 className='bg-yellow-500 font-extralight font-bold'>Hello World</h2>
-    </>
+    <div>
+      <h1 className="head-text">ThreeJS</h1>
+    </div>
   )
 }
 
 export default App
+ 
