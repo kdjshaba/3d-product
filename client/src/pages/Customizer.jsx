f6dcf3a38c215801e31b96109c1591e861483d2c
Rahul <rahulshelke4172@gmail.com>
2023-08-31 23:47:03 +0530

    updated customizer and canvas

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -26,9 +26,41 @@ const Customizer = () => {
            >
             <div className="flex items-centre min-h-screen">
               <div className="editortabs-container tabs">
-                
+                {EditorTabs.map((tab) => (
+                 <Tab 
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                 />
+                ))}
               </div>
             </div>
+           </motion.div>
+           <motion.div className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+           >
+            <CustomButton 
+              type="filled"
+              title="Go Back"
+              handleClick={() => state.intro = true}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+
+           </motion.div>
+           <motion.div
+             className='filtertabs-container'
+             {...slideAnimation('up')}
+           >
+          {FilterTabs.map((tab) => (
+                 <Tab 
+                    key={tab.name}
+                    tab={tab}
+                    isFilterTab
+                    isActiveTab=""
+                    handleClick={() => {}}
+                 />
+                ))}
+
            </motion.div>
         </>
       )}
