d2c3dbdc3657d2999e6aec873ca0b7d33ae8c14e
Rahul <rahulshelke4172@gmail.com>
2023-09-06 23:31:48 +0530

    changes

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,5 +1,5 @@
-import  {Canvas} from '@react-three/fiber';
-import {Environment , Center} from '@react-three/drei';
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
@@ -7,15 +7,21 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5}/>
-      <Environment preset='city'/>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
       <CameraRig>
         <Backdrop />
         <Center>
-          <Shirt />  
-        </Center>  
-      </CameraRig>   
+          <Shirt />
+        </Center>
+      </CameraRig>
     </Canvas>
   )
 }
